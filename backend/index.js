e12b11a9fefbfc85c65a6b81775358e8346c34cf
Rahul Gala <rgala38@gmail.com>
2020-08-11 15:26:11 +0530

    Chat app comeplete

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const io = require("socket.io")(8000);
+
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", { message, name: users[socket.id] });
+  });
+
+  socket.on("disconnect", (reason) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+});
